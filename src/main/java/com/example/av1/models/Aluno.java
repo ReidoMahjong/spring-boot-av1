86dc5aed2152ecbaea5c867dd891893178c72f3f
Igor Miguel <160028031+ReidoMahjong@users.noreply.github.com>
2025-09-04 09:59:35 -0300

    Add files via upload

---
+++ src/main/java/com/example/av1/models/Aluno.java
@@ -0,0 +1,7 @@
+package com.example.av1.models;
+
+import java.util.Objects;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
